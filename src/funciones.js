4ff3405647572a418746f4a8e9d6a90a2f8981b1
Piwexx <vigarioignacio@gmail.com>
2025-04-24 19:41:31 -0300

    first commit

---
+++ src/funciones.js
@@ -0,0 +1,18 @@
+export const sumar = (a, b) => a + b;
+
+export const restar = (a, b) => a - b;
+
+export const multiplicar = (a, b) => a * b;
+
+export const dividir = (a, b) => {
+  if (b === 0) {
+    return "No se puede dividir entre cero";
+  }
+  return a / b;
+};
+
+export const potencia = (base, exponente) => Math.pow(base, exponente);
+
+export const seno = (angulo) => Math.sin(angulo);
+
+export const raizCuadrada = (y) => Math.sqrt(y);
